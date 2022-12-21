864bd39c82cec9409b6b550f3130b7e20174efe5
Cesare-Tomasello <cesarinho@live.it>
2022-12-21 17:54:54 +0100

    esercitazione 21-12-22

---
+++ 21-12-22/src/component/button/Button.jsx
@@ -0,0 +1,12 @@
+import "./Button.css";
+
+export function Button(props) {
+  const { className = "", variant = "none", ...otherAttributes } = props;
+
+  return (
+    <button
+      className={`button button--variant-${variant} ${className}`}
+      {...otherAttributes}
+    ></button>
+  );
+}
